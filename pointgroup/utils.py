0c04e88260e569e81ea1951b8a3475563cc74cc4
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2025-07-13 01:08:13 +0900

    Update utils.py

--- pointgroup/utils.py
+++ pointgroup/utils.py
@@ -2,7 +2,7 @@ from ase.io import write
 from ase.data import chemical_symbols
 from ase import Atoms
 
-def get_cif(cell, pos, species, name="vis.cif"):
+def visualize(cell, pos, species, name="vis.cif"):
     symbols = [chemical_symbols[Z] if isinstance(Z, int) else Z for Z in species]
     atoms = Atoms(symbols=symbols, positions=positions, cell=cell, pbc=True)
     write(name, atoms, format='cif')
