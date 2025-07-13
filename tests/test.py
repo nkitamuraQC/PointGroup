8d9cb158da51e983ce42e3b12257a1e1fe41b669
Naoki Kitamura <nkitamura1234@gmail.com>
2025-07-13 20:44:39 +0900

    characterテスト追加

--- tests/test.py
+++ tests/test.py
@@ -8,29 +8,55 @@ from pointgroup import (
 import numpy as np
 
 def test_overall():
-    lat=[[1.0, 0, 0],[0, 1.0, 0],[0, 0, 1.0]]
-    orb=[[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]
+    # Buckled layer
+    # real space is 3D
+    # define lattice vectors
+    lat=[[1.0,0.0,0.0],[0.0,1.25,0.0],[0.0,0.0,3.0]]
+    # define coordinates of orbitals
+    orb=[[0.0,0.0,-0.15],[0.5,0.5,0.15]]
     site_species = [1, 2]
-    my_model=tb_model(3, 3, lat, orb)
-    my_model.set_hop(-1., 0, 0, [1, 0, 0])
+    
+    # only first two lattice vectors repeat, so k-space is 2D
+    my_model=tb_model(3,3,lat,orb)
+    
+    # set model parameters
+    delta=1.1
+    t=0.6
+    
+    # set on-site energies
+    my_model.set_onsite([-delta,delta])
+    # set hoppings (one for each connected pair of orbitals)
+    # (amplitude, i, j, [lattice vector to cell containing j])
+    my_model.set_hop(t, 1, 0, [0, 0, 0])
+    my_model.set_hop(t, 1, 0, [1, 0, 0])
+    my_model.set_hop(t, 1, 0, [0, 1, 0])
+    my_model.set_hop(t, 1, 0, [1, 1, 0])
 
     tb = TBModel(pythtb_obj=my_model, site_species=site_species)
     tb.gen_pythtb()
 
     gcclass = GetCharacter(tb)
-    ch = gcclass.get_character(kidx=0, orb_idx=0, op_idx=1)
-    print(ch)
+    gcclass = GetCharacter(tb)
+    n = gcclass._get_symm_ops()
+    res = []
+    for i in range(n):
+        ch = gcclass.get_character(kidx=0, orb_idx=1, op_idx=i)
+        res.append(ch)
+    print(res.count(1))
+    print(res.count(-1))
     ir = GetIR(gcclass)
     h = ir._get_h()
     ch = np.ones((h))
-    ir.reduce_ir(ch, ir_idx=0)
+    w_ch = ir.reduce_ir(ch, ir_idx=0)
+    print(w_ch)
 
-    salc = GetSALC(gcclass)
-    w = salc.make_salc_site(sym_idx=0)
-    print(w)
+    # salc = GetSALC(gcclass)
+    # w = salc.make_salc_site(sym_idx=0)
+    # print(w)
     return
 
 def test_overall2():
+    # graphene
     # define lattice vectors
     lat=[[1.0,0.0,0.0],[0.5,np.sqrt(3.0)/2.0,0.0],[0.0,0.0,10.0]]
     # define coordinates of orbitals
@@ -73,6 +99,56 @@ def test_overall2():
         print(w)
     return
 
+def test_overall3():
+    # Checkerboard
+    # define lattice vectors
+    lat=[[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,10.0]]
+    # define coordinates of orbitals
+    orb=[[0.0,0.0,0.0],[0.5,0.5,0.0]]
+    
+    # make two dimensional tight-binding checkerboard model
+    my_model=tb_model(3,3,lat,orb)
+    
+    # set model parameters
+    delta=1.1
+    t=0.6
+    
+    # set on-site energies
+    my_model.set_onsite([-delta,delta])
+    # set hoppings (one for each connected pair of orbitals)
+    # (amplitude, i, j, [lattice vector to cell containing j])
+    my_model.set_hop(t, 1, 0, [0, 0, 0])
+    my_model.set_hop(t, 1, 0, [1, 0, 0])
+    my_model.set_hop(t, 1, 0, [0, 1, 0])
+    my_model.set_hop(t, 1, 0, [1, 1, 0])
+
+    site_species = [1, 2]
+
+    tb = TBModel(pythtb_obj=my_model, site_species=site_species)
+    tb.gen_pythtb()
+
+    gcclass = GetCharacter(tb)
+    gcclass = GetCharacter(tb)
+    n = gcclass._get_symm_ops()
+    res = []
+    for i in range(n):
+        ch = gcclass.get_character(kidx=0, orb_idx=1, op_idx=i)
+        res.append(ch)
+    print(res.count(1))
+    print(res.count(-1))
+    ir = GetIR(gcclass)
+    h = ir._get_h()
+    ch = np.ones((h))
+    w_ch = ir.reduce_ir(ch, ir_idx=0)
+    print(w_ch)
+
+    # salc = GetSALC(gcclass)
+    # w = salc.make_salc_site(sym_idx=0)
+    # print(w)
+    return
+
 
 if __name__ == "__main__":
-    test_overall2()
+    # test_overall()
+    # test_overall2()
+    test_overall3()
